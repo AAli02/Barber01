eb8f60c1ee884a070b119cfcaa578bfdf565d4b5
AAli02 <aali.02p@gmail.com>
2023-04-08 15:28:06 -0400

    add

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,51 +1,48 @@
-import React from 'react'
-import './Cards.css'
+import React from "react";
+import img1 from "../../assets/port2.png";
+import img2 from "../../assets/port3.png";
+
+import "./Cards.css";
 
 function Cards() {
   return (
-    <div className="card-section">
-  <div className="card">
-    <div className="card-front">
-      <img src="" alt="card-front" />
-    </div>
-    <div className="card-back">
-      <h3>Card Title</h3>
-      <p>Card description goes here.</p>
-      <a href="#">Learn More</a>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-front">
-      <img src="" alt="card-front" />
-    </div>
-    <div className="card-back">
-      <h3>Card Title</h3>
-      <p>Card description goes here.</p>
-      <a href="#">Learn More</a>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-front">
-      <img src="" alt="card-front" />
-    </div>
-    <div className="card-back">
-      <h3>Card Title</h3>
-      <p>Card description goes here.</p>
-      <a href="#">Learn More</a>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-front">
-      <img src="" alt="card-front" />
-    </div>
-    <div className="card-back">
-      <h3>Card Title</h3>
-      <p>Card description goes here.</p>
-      <a href="#">Learn More</a>
-    </div>
-  </div>
-</div>
-  )
+    <body>
+      <div className="card-container">
+        <div className="card-section">
+          <div className="card">
+            <div className="card-front">
+              <img src={img1} alt="card-front" />
+            </div>
+            <div className="card-back">
+              <h3>Card Title</h3>
+              <p>Card description goes here.</p>
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-front">
+              <img src={img2} alt="card-front" />
+            </div>
+            <div className="card-back">
+              <h3>Card Title</h3>
+              <p>Card description goes here.</p>
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-front">
+              <img src={img1} alt="card-front" />
+            </div>
+            <div className="card-back">
+              <h3>Card Title</h3>
+              <p>Card description goes here.</p>
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
 }
 
-export default Cards
+export default Cards;
